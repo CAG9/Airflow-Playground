5b930d1235fb86f3206b49e6229c0bcb9bcd8d16
Cesar Arcos Gonzalez <60860385+CAG9@users.noreply.github.com>
2026-01-07 16:24:26 -0600

    Update ecom.py

--- Part2/ecom.py
+++ Part2/ecom.py
@@ -1,13 +1,14 @@
 from airflow.decorators import dag
 from airflow.operators.empty import EmptyOperator
 from pendulum import datetime, duration
+from include.datasets import DATASET_COCKTAIL
 
 @dag(start_date=datetime(2025, 1 ,1),
-     schedule='@weekly',
+     schedule=[DATASET_COCKTAIL],
      catchup=True,
      description="This DAG processes ecommerce data",
      tags=["team_a", "ecom"],
-     default_args={"retries": 1},
+     default_args={"retries": 2},
      dagrun_timeout=duration(minutes=20),
      max_consecutive_failed_dag_runs=2)
 def ecom():
@@ -15,4 +16,5 @@ def ecom():
     ta = EmptyOperator(task_id='ta')
     
 ecom()
+
 
