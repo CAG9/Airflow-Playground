1834e9f615d7a4719e0174e358146dcbb104213b
Cesar Arcos Gonzalez <60860385+CAG9@users.noreply.github.com>
2026-01-07 16:25:04 -0600

    Update extractor.py

--- Part2/extractor.py
+++ Part2/extractor.py
@@ -1,41 +1,40 @@
 from airflow.decorators import dag
 from airflow.operators.python import PythonOperator
-from pendulum import datetime
-from airflow.datasets import Dataset
-
-DATASET_COCKTAIL = Dataset('/tmp/cocktail.json')
-
-def _get_cocktail(ti=None):
-    import requests
-    
-    api = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-    response = requests.get(api)
-    with open(DATASET_COCKTAIL.uri, "wb") as f:
-        f.write(response.content)
-    ti.xcom_push(key='request_size', value=len(response.content))
-        
-def _check_size(ti=None):
-    size = ti.xcom_pull(key='request_size', task_ids='get_cocktail')
-    print(size)
+from pendulum import datetime, duration
+from include.datasets import DATASET_COCKTAIL
+from include.tasks import _get_cocktail, _check_size
+from include.extractor.callbacks import _handle_empty_size, _handle_failed_dag_run
 
 @dag(
     start_date=datetime(2025, 1, 1),
     schedule='@daily',
-    catchup=False
+    default_args={
+        'retries': 2,
+        'retry_delay': duration(seconds=2),
+    },
+    tags=['ecom'],
+    catchup=False,
+    on_failure_callback=_handle_failed_dag_run
 )
 def extractor():
     
     get_cocktail = PythonOperator(
         task_id='get_cocktail',
         python_callable=_get_cocktail,
-        outlets=[DATASET_COCKTAIL]
+        outlets=[DATASET_COCKTAIL],
+        retry_exponential_backoff=True,
+        max_retry_delay=duration(minutes=15)
     )
     
     check_size = PythonOperator(
         task_id='check_size',
-        python_callable=_check_size
+        python_callable=_check_size,
+        on_failure_callback=_handle_empty_size
     )
     
     get_cocktail >> check_size
     
-extractor()
+my_extractor = extractor()
+
+if __name__ == "__main__":
+    my_extractor.test()
